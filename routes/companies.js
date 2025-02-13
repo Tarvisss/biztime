a00104549bd422ee43b0fd89c394217a4252ac48
Travis Souther <souther-trav@hotmail.com>
2025-02-12 19:00:55 -0600

    added biztime-ex

---
+++ routes/companies.js
@@ -0,0 +1,66 @@
+const express = require("express");
+const router = express.Router();
+const db = require("../db");
+
+
+router.get('/', async(req, res, next) =>{
+    try{
+    const results = await db.query('SELECT * FROM companies');
+    return res.json(results.rows);
+       } catch (error){
+         return next(error);
+       } 
+})
+
+
+router.get('/:code', async (req, res, next) => {
+    try {
+        const { code } = req.params;
+        const results = await db.query('SELECT * FROM companies WHERE code = $1', [code]);
+
+        if (results.rows.length === 0) {
+            return res.status(404).json({ message: "Company not found" });
+        }
+
+        return res.json(results.rows[0]);
+    } catch (error) {
+        return next(error);
+    }
+});
+router.post('/', async (req, res, next) => {
+    try {
+        const { code, name } = req.body;
+        const results = await db.query('INSERT INTO companies (code, name) VALUES ($1,$2) RETURNING *', [code, name] );
+        return res.status(201).json(results.rows[0]);
+    } catch (error) {
+        return next(error)
+    }
+})
+
+router.put('/:code', async (req, res, next) => {
+    try {
+      const { code } = req.params;
+      const { name, newCode } = req.body;
+      const results = await db.query('UPDATE companies SET name=$1, code=$2 WHERE code=$3 RETURNING name, code', [name, newCode, code])
+      if (results.rows.length === 0) {
+        throw new ExpressError(`Can't update company with code of ${code}`, 404)
+      }
+      return res.send({ companies: results.rows[0] })
+    } catch (error) {
+      return next(error)
+    }
+  })
+  router.delete('/:code', async (req, res, next) => {
+    try{
+        const results = db.query('DELETE FROM companies WHERE code = $1', [req.params.code])
+        return res.send({message: "Deleted"})
+    } catch (error) {
+      return next(error)  
+    }
+  })
+
+
+
+
+
+module.exports = router;
---
+++ routes/invoices.js
@@ -0,0 +1,69 @@
+const express = require("express");
+const router = express.Router();
+const db = require("../db");
+
+
+router.get('/', async(req, res, next) =>{
+    try{
+    const results = await db.query('SELECT * FROM invoices');
+    return res.json(results.rows);
+       } catch (error){
+         return next(error);
+       } 
+})
+
+
+router.get('/:id', async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const results = await db.query(
+            'SELECT * FROM invoices WHERE id = $1', [id]);
+
+        if (results.rows.length === 0) {
+            return res.status(404).json({ message: "invoice not found" });
+        }
+
+        return res.json(results.rows[0]);
+    } catch (error) {
+        return next(error);
+    }
+});
+router.post('/', async (req, res, next) => {
+    try {
+         // Extract comp_code and amt from request body
+        const { comp_code, amt } = req.body;
+        const results = await db.query(
+            'INSERT INTO invoices ( comp_code, amt) VALUES ($1,$2) RETURNING *', [comp_code, amt] );
+        return res.status(201).json(results.rows[0]);
+    } catch (error) {
+        return next(error)
+    }
+})
+// I would like to go over this more /////////
+router.put('/:id', async (req, res, next) => {
+    try {
+      const { id } = req.params; // Extract the id from the URL parameter
+      const { amt } = req.body; // Extract amt from the request body
+      const results = await db.query(
+        `UPDATE invoices 
+         SET amt = $1 
+         WHERE id = $2
+         RETURNING * `, [amt, id]);
+      if (results.rows.length === 0) {
+        throw new ExpressError(`invoice not found`, 404)
+      }
+      return res.send({ invoices: results.rows[0] })
+    } catch (error) {
+      return next(error)
+    }
+  })
+  router.delete('/:id', async (req, res, next) => {
+    try{
+        const results = db.query('DELETE FROM invoices WHERE id = $1', [req.params.id])
+        return res.send({message: "Deleted"})
+    } catch (error) {
+      return next(error)  
+    }
+  })
+
+module.exports = router;
